5ea9652c7b58be62b740b46336bdc2c744fdcc9d
Yulia <yudemins@mail.ru>
2023-03-24 23:50:00 +0300

    feat: add tests for form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
@@ -12,12 +12,13 @@ import { About } from './pages/about/About';
 import { Search } from './components/search/Search';
 import { NotFound } from './pages/notFound/NotFound';
 import { Title } from './components/title/Title';
+import { Form } from './pages/form/Form';
+import { FormData } from './components/formData/FormData';
+import { FormValue } from './components/formValue/FormValue';
+import { Modal } from './components/modal/Modal';
+import { Layout } from './components/layout/Layout';
 
 describe('App', () => {
-  it('renders cards', () => {
-    render(<Cards />);
-    expect(screen.getByTestId(/сards/i)).toBeInTheDocument();
-  });
   it('renders title, author in cards', () => {
     const id = 0;
     const src = 'Test url';
@@ -82,5 +83,55 @@ describe('App', () => {
     const button = screen.getByLabelText('search');
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+  it('renders Form', () => {
+    render(<Form />);
+    expect(screen.getByText(/form/i)).toBeInTheDocument();
+  });
+  it('check FormData', () => {
+    render(<FormData />);
+    fireEvent.change(screen.getByLabelText(/name/i), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/country/i), { target: { value: 'Spain' } });
+    fireEvent.change(screen.getByLabelText(/birthday/i), { target: { value: '12.03.2022' } });
+    userEvent.click(screen.getByText('Submit'));
+  });
+  it('render FormValue', () => {
+    const imageValue = 'test.png';
+    const nameValue = 'Test';
+    const countryValue = 'Spain';
+    const birthdayValue = '12.03.2022';
+    const genderValue = 'male';
+    const agree = true;
+
+    render(
+      <FormValue
+        key={1}
+        values={{
+          imageValue,
+          nameValue,
+          countryValue,
+          birthdayValue,
+          genderValue,
+          agree,
+        }}
+      />
+    );
+    expect(screen.getByText(/name/i)).toBeInTheDocument();
+    expect(screen.getByText(/country/i)).toBeInTheDocument();
+    expect(screen.getByText(/birthday/i)).toBeInTheDocument();
+    expect(screen.getByText(/gender/i)).toBeInTheDocument();
+    expect(screen.getByText(/agree/i)).toBeInTheDocument();
+  });
+  it('renders modal', () => {
+    render(<Modal />);
+    expect(screen.getByText(/данные отправлены/i)).toBeInTheDocument();
+  });
+  it('renders main', () => {
+    render(
+      <MemoryRouter>
+        <Layout title="Home" content={'Home'} />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('main')).toBeInTheDocument();
   });
 });
