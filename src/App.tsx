b8193a5f83e2bd4e72422d98fbe14ece0e1759b7
Yulia <yudemins@mail.ru>
2023-04-02 16:48:00 +0300

    feat: update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import './App.css';
-
 import { About } from './pages/about/About';
 import { Home } from './pages/home/Home';
 import { NotFound } from './pages/notFound/NotFound';
-
 import { Layout } from './components/layout/Layout';
 import { Form } from './pages/form/Form';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="form" element={<Form />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <RouterProvider
+      router={createBrowserRouter(
+        createRoutesFromElements(
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="form" element={<Form />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        )
+      )}
+    />
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,11 @@ import { useLocation } from 'react-router-dom';
 import { Nav } from '../nav/Nav';
 import styles from './header.module.css';
 
-type pathNameType = {
+type pathType = {
   [key: string]: string;
 };
 
-const pathName: pathNameType = { '/': 'Home', '/about': 'About', '/form': 'Form', '/*': '404' };
+const pathName: pathType = { '/': 'Home', '/about': 'About', '/form': 'Form', '/*': '404' };
 
 export const Header = () => {
   const { pathname } = useLocation();
