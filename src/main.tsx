e8685efe6b24573abc73738d5b38e4cc1edcadc6
Yulia <yudemins@mail.ru>
2023-05-28 16:10:46 +0300

    feat: add persist for localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import './index.css';
-import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/store';
+import { Preloader } from './components/preloader/Preloader';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<Preloader />} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
